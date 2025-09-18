326cc48da51c6e40c0cccca245d17500c5c196ba
William Falls <wfallmc@gmail.com>
2025-09-18 23:51:51 +0900

    Add files via upload

---
+++ src/assets/images/index.js
@@ -0,0 +1,281 @@
+const Apple = require('./icons/Apple.png');
+const Google = require('./icons/Google.png');
+const Facebook = require('./icons/Facebook.png');
+const Logo = require('./icons/Logo.png');
+const Check = require('./icons/Check.png');
+const EyeOff = require('./icons/EyeOff.png');
+const EyeOn = require('./icons/EyeOn.png');
+const Phone = require('./icons/Phone.png');
+const Email = require('./icons/Email.png');
+const USFlag = require('./icons/USFlag.png');
+const Snapchat = require('./icons/Snapchat.png');
+const Twitter = require('./icons/Twitter.png');
+const Instagram = require('./icons/Instagram.png');
+const TikTok = require('./icons/TikTok.png');
+const Upload = require('./icons/Upload.png');
+const ChevronDown = require('./icons/ChevronDown.png');
+const Desclaimer = require('./icons/Desclaimer.png');
+const BackOrange = require('./icons/BackOrange.png');
+const Search = require('./icons/Search.png');
+const Notification = require('./icons/Notification.png');
+const Profile = require('./icons/Profile.png');
+const Menu = require('./icons/Menu.png');
+const PoolParty = require('./icons/Cat1.png');
+const BirthdayParty = require('./icons/Cat2.png');
+const GardenParty = require('./icons/Cat3.png');
+const DinnerParty = require('./icons/Cat4.png');
+const TeaParty = require('./icons/Cat5.png');
+const CocktailParty = require('./icons/Cat6.png');
+const Rating = require('./icons/Rating.png');
+const Location = require('./icons/Location.png');
+const List = require('./icons/List.png');
+const Grid = require('./icons/Grid.png');
+const Filter = require('./icons/Filter.png');
+
+const Onboarding1 = require('./backgrounds/Onboarding1.png');
+const Onboarding2 = require('./backgrounds/Onboarding2.png');
+const Onboarding3 = require('./backgrounds/Onboarding3.png');
+const SocialBG = require('./backgrounds/SocialBG.png');
+const ModalBG = require('./backgrounds/ModalBG.png');
+const VerificationImage = require('./backgrounds/VerificationImage.png');
+const TabBar = require('./backgrounds/TabBar.png');
+const SocialLogo = require('./backgrounds/SocialLogo.png');
+const PostCardBG = require('./backgrounds/PostCardBG.png');
+// const Home = require('./tabicons/Home.png');
+const Chat = require('./tabicons/Chat.png');
+const Create = require('./tabicons/Create.png');
+
+// About, ContactUs, FAQ, Privacy, Terms, Profile, Settings, Parties
+const About = require('./drawerIcons/About.png');
+const AboutFocused = require('./drawerIcons/AboutFocused.png');
+const ContactUs = require('./drawerIcons/ContactUs.png');
+const ContactUsFocused = require('./drawerIcons/ContactUsFocused.png');
+const FAQ = require('./drawerIcons/FAQ.png');
+const FAQFocused = require('./drawerIcons/FAQFocused.png');
+const Privacy = require('./drawerIcons/Privacy.png');
+const PrivacyFocused = require('./drawerIcons/PrivacyFocused.png');
+const Terms = require('./drawerIcons/Terms.png');
+const TermsFocused = require('./drawerIcons/TermsFocused.png');
+const ProfileIcon = require('./drawerIcons/Profile.png');
+const ProfileFocused = require('./drawerIcons/ProfileFocused.png');
+const Settings = require('./drawerIcons/Settings.png');
+const SettingsFocused = require('./drawerIcons/SettingsFocused.png');
+const Parties = require('./drawerIcons/Parties.png');
+const PartiesFocused = require('./drawerIcons/PartiesFocused.png');
+const Welcome = require('./backgrounds/Welcome.png');
+const Screenshot = require('./backgrounds/Screenshot.png');
+const MyAds = require('./drawerIcons/MyAds.png')
+const Logout = require('./drawerIcons/Logout.png')
+import Male from './icons/Male.png';
+import Female from './icons/Female.png';
+import PreferNotToSay from './icons/Other.png';
+import Happy from './icons/Happy.png';
+import Sad from './icons/Sad.png';
+import Rough from './icons/Rough.png';
+import Tick from './icons/Tick.png';
+import Cross from './icons/Cross.png';
+import Marker from './icons/Marker.png';
+import RightMenu from './icons/RightMenu.png';
+import Send from './icons/Send.png';
+import AddYellow from './icons/AddYellow.png';
+import Delete from './icons/Delete.png';
+import Edit from './icons/Edit.png';
+import ArrowDown from './icons/ArrowDown.png';
+import SolidSend from './icons/SolidSend.png';
+import Warning from './icons/Warning.png';
+import PopupWaring from './icons/PopupWarning.png';
+import PopupCheck from './icons/PopupCheck.png';
+import SolidSave from './icons/SolidSave.png';
+import Wishlist from './icons/Wishlist.png';
+import Heart from './icons/Heart.png';
+import View from './icons/View.png';
+import Camera from './icons/Camera.png';
+import Website from './icons/Website.png';
+import Event from './icons/Event.png';
+import Guests from './icons/Guests.png';
+import OutlineLocation from './icons/OutlineLocation.png';
+import Calendar from './icons/Calendar.png';
+import Tag from './icons/Tag.png';
+import All from './icons/All.png';
+import AddEvent from './icons/AddEvent.png';
+import Add from './icons/Add.png';
+import Notes from './icons/Notes.png';
+import Expenses from './icons/Expenses.png';
+import Outfit from './icons/Outfit.png';
+import EditProfile from './icons/EditProfile.png';
+import QuestionPopup from './icons/QuestionPopup.png';
+import AddGuest from './icons/AddGuest.png';
+import AddNote from './icons/AddNote.png';
+import CardChat from './icons/CardChat.png';
+import CardCall from './icons/CardCall.png';
+import AddAd from './icons/AddAd.png';
+import LostFound from './icons/LostFound.png';
+import ClassifiedIcon from './icons/Classified.png';
+import ProfilePlaceholder from './backgrounds/ProfilePlaceholder.jpeg';
+// Tabs
+import Home from './tabicons/Home.png';
+import ToDo from './tabicons/ToDo.png';
+import Guest from './tabicons/Guest.png';
+import GuestFocused from './tabicons/GuestFocused.png';
+import ChatFocused from './tabicons/ChatFocused.png';
+import HomeFocused from './tabicons/HomeFocused.png';
+import ToDoFocused from './tabicons/ToDoFocused.png';
+import Services from './tabicons/Services.png';
+import ServicesFocused from './tabicons/ServicesFocused.png';
+import UserFocused from './tabicons/UserFocused.png';
+import User from './tabicons/User.png';
+import Classified from './tabicons/Classified.png';
+
+// Drawer
+import MyPosts from './drawerIcons/MyPosts.png';
+import MyThread from './drawerIcons/MyThreads.png';
+import Story from './drawerIcons/Story.png';
+import Logs from './drawerIcons/Logs.png';
+import Saved from './drawerIcons/Saved.png';
+import Support from './drawerIcons/Support.png';
+import SubLog from './drawerIcons/SubLog.png';
+import Review from './drawerIcons/Review.png';
+export const Icons = {
+  Apple,
+  Google,
+  Facebook,
+  Logo,
+  Check,
+  EyeOff,
+  EyeOn,
+  Phone,
+  Email,
+  USFlag,
+  Snapchat,
+  Twitter,
+  Instagram,
+  TikTok,
+  Upload,
+  ChevronDown,
+  Desclaimer,
+  BackOrange,
+  Search,
+  Notification,
+  Profile,
+  Menu,
+  PoolParty,
+  BirthdayParty,
+  GardenParty,
+  DinnerParty,
+  TeaParty,
+  CocktailParty,
+  Rating,
+  Location,
+  List,
+  Grid,
+  Filter,
+  Male,
+  Female,
+  PreferNotToSay,
+  Happy,
+  Sad,
+  Rough,
+  Tick,
+  Cross,
+  Marker,
+  RightMenu,
+  Send,
+  AddYellow,
+  Delete,
+  Edit,
+  ArrowDown,
+  SolidSend,
+  Warning,
+  PopupWaring,
+  PopupCheck,
+  SolidSave,
+  Wishlist,
+  Heart,
+  CardChat,
+  View,
+  Camera,
+  Website,
+  Event,
+  Guests,
+  OutlineLocation,
+  Calendar,
+  Tag,
+  All,
+  AddEvent,
+  EditProfile,
+  Add,
+  CardCall,
+  QuestionPopup,
+  AddGuest,
+  AddNote,
+  AddAd
+};
+
+export const Backgrounds = {
+  Onboarding1,
+  Onboarding2,
+  Onboarding3,
+  SocialBG,
+  ModalBG,
+  VerificationImage,
+  TabBar,
+  Welcome,
+  Screenshot,
+  SocialLogo,
+  PostCardBG,
+  ClassifiedIcon,
+  LostFound,
+  ProfilePlaceholder
+};
+
+export const TabIcons = {
+  Home,
+  Chat,
+  Create,
+  ToDo,
+  Guest,
+  GuestFocused,
+  ChatFocused,
+  HomeFocused,
+  ToDoFocused,
+  Services,
+  ServicesFocused,
+  User,
+  UserFocused,
+  Classified
+};
+
+export const DrawerIcons = {
+  Home: HomeFocused,
+  About,
+  AboutFocused,
+  ContactUs,
+  ContactUsFocused,
+  FAQ,
+  FAQFocused,
+  Privacy,
+  PrivacyFocused,
+  Terms,
+  TermsFocused,
+  Profile: ProfileIcon,
+  ProfileFocused,
+  Settings,
+  SettingsFocused,
+  Parties,
+  PartiesFocused,
+  MyPosts,
+  MyThread,
+  Story,
+  Logs,
+  Saved,
+  Support,
+  Camera,
+  Notes,
+  Expenses,
+  Event,
+  Outfit,
+  SubLog,
+  Review,
+  MyAds,
+  Logout
+};
